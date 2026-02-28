9d3fadbe1e6f6a6abeefd051bc7c6e4a335fd431
Bhoomi Chauhan <123720183+bhoomi018@users.noreply.github.com>
2026-02-28 11:19:50 +0530

    Create error.middleware.ts

---
+++ backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+
+export function errorMiddleware(
+  err: any,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  console.error(err);
+
+  res.status(400).json({
+    success: false,
+    message: err.message || "Something went wrong"
+  });
+}
