911dced3603a759df0aa51f55f66f5c78f1780cd
Bhoomi Chauhan <123720183+bhoomi018@users.noreply.github.com>
2026-02-28 11:18:21 +0530

    Update widget.service.ts

--- backend/src/modules/widget/widget.service.ts
+++ backend/src/modules/widget/widget.service.ts
@@ -1,5 +1,12 @@
 import { generateMockData } from "../../utils/mockDataGenerator";
+import { WidgetTypes } from "../../constants/widgetTypes";
 
 export function getWidgetData(type: string) {
+  const validTypes = Object.values(WidgetTypes);
+
+  if (!validTypes.includes(type as any)) {
+    throw new Error("Invalid widget type");
+  }
+
   return generateMockData(type);
 }
