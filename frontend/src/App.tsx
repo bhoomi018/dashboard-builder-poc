64ca27813d0e4a1ea0dd74d3b1b9d39be852f4a9
Bhoomi Chauhan <123720183+bhoomi018@users.noreply.github.com>
2026-02-28 11:26:26 +0530

    Add App component with DashboardShell

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import DashboardShell from "./features/dashboard/DashboardShell";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Dashboard Builder</h1>
+      <DashboardShell />
+    </div>
+  );
+}
+
+export default App;
