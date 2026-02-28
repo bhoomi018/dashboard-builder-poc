c8f62b2e12829bcb770f3081e20b90b3b2e9e296
Bhoomi Chauhan <123720183+bhoomi018@users.noreply.github.com>
2026-02-28 11:37:33 +0530

    Create ErrorBoundary.tsx

---
+++ frontend/src/components/ErrorBoundary.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class ErrorBoundary extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Widget crashed</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
